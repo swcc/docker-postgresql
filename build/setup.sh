a8f174d0b0177b4abb1e8c1ad90625fe961a10bf
Paul B <paul.bonaud@capitainetrain.com>
2015-01-12 01:01:11 +0100

    Create Data dir for PG if it does not exist
    Conflicts:
    	build/setup.sh

--- build/setup.sh
+++ build/setup.sh
@@ -1,18 +1,27 @@
 #!/bin/sh
 
-# Start postgresql
-/usr/bin/pg_ctlcluster "9.4" main start
+# If empty data directory
+if [ ! -f /var/lib/postgresql/9.4/main/PG_VERSION ]
+then
+    # Create postgres data directory
+    mkdir -p /var/lib/postgresql/9.4/main
+    chown postgres:postgres /var/lib/postgresql/9.4/main
+    /sbin/setuser postgres /usr/lib/postgresql/9.4/bin/initdb /var/lib/postgresql/9.4/main/
 
-# Create users and databases here
-/sbin/setuser postgres createdb mydb
-/sbin/setuser postgres createuser -DRS myuser
-/sbin/setuser postgres psql -c 'GRANT ALL PRIVILEGES ON DATABASE mydb TO myuser;'
-# Give access to outside world with password auth
-echo "host    all             all             172.17.0.0/16           md5
+    # Start postgresql
+    /usr/bin/pg_ctlcluster "9.4" main start
+
+    # Create users and databases here
+    /sbin/setuser postgres createdb mydb
+    /sbin/setuser postgres createuser -DRS myuser
+    /sbin/setuser postgres psql -c 'GRANT ALL PRIVILEGES ON DATABASE mydb TO myuser;'
+    # Give access to outside world with password auth
+    echo "host    all             all             172.17.0.0/16           md5
 " >> /etc/postgresql/9.4/main/pg_hba.conf
 
-# Stop postgresql
-/usr/bin/pg_ctlcluster "9.4" main stop
+    # Stop postgresql
+    /usr/bin/pg_ctlcluster "9.4" main stop
+fi
 
 # Launch init process
 /sbin/my_init
